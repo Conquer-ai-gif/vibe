6e86ee035ae3f3afe5872e1252326cddd50fddd8
Conquer-ai-gif <korleylahm@gmail.com>
2025-12-08 20:28:41 -0800

    06 e2b sanboxes

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,11 +1,18 @@
+import {Sandbox} from "@e2b/code-interpreter";
 import { openai, createAgent } from "@inngest/agent-kit";
 
 import { inngest } from "./client";
+import { getSandbox } from "./utils";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event }) => {
+  async ({ event,step }) => {
+  const sandboxId = await step.run('get-sandbox-id', async () => {
+    const sandbox = await Sandbox.create('sandbox id')
+    return sandbox.sandboxId;
+  });
+
     // Create a new agent with a system prompt (you can add optional tools, too)
     const codeAgent = createAgent({
       name: "code-agent",
@@ -18,7 +25,15 @@ export const helloWorld = inngest.createFunction(
     const { output } = await codeAgent.run(`sumarize the following text:${+ event.data.value} `);
 
     console.log(output)
+
+    const sandboxUrl = await step.run('get-sandbox-url', async () => {
+
+      const sandbox =await getSandbox(sandboxId);
+      const host= sandbox.getHost(3000);
+      return `https://${host}`;
+    })
+
   
-    return {output};
+    return {output,sandboxUrl};
   },
 );
