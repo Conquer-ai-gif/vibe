37ec51362dfe6138df6fa08d4b2fc6e028d4ef0b
Conquer-ai-gif <korleylahm@gmail.com>
2025-12-10 21:23:34 -0800

    08 messages

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,15 +1,21 @@
 import { z } from "zod";
 import {Sandbox} from "@e2b/code-interpreter";
-import { openai, createAgent,createTool,createNetwork } from "@inngest/agent-kit";
+import { openai, createAgent,createTool,createNetwork,type Tool } from "@inngest/agent-kit";
 
 import { PROMPT } from "@/prompt";
+import { prisma } from "@/lib/db";
 
 import { inngest } from "./client";
 import { getSandbox,lastAssistantTextMassageContent } from "./utils";
 
-export const helloWorld = inngest.createFunction(
-  { id: "hello-world" },
-  { event: "test/hello.world" },
+interface AgentState {
+  summary:string;
+  files: { [path:string]: string};
+};
+
+export const codeAgentFunction = inngest.createFunction(
+  { id: "code-agent" },
+  { event: "code-agent/run" },
   async ({ event,step }) => {
   const sandboxId = await step.run('get-sandbox-id', async () => {
     const sandbox = await Sandbox.create('sandbox id')
@@ -17,7 +23,7 @@ export const helloWorld = inngest.createFunction(
   });
 
     // Create a new agent with a system prompt (you can add optional tools, too)
-    const codeAgent = createAgent({
+    const codeAgent = createAgent<AgentState>({
       name: "code-agent",
       description: "An expert coding agent",
       system: PROMPT,
@@ -65,7 +71,7 @@ export const helloWorld = inngest.createFunction(
               }),
             ),
           }),
-          handler: async( {files},{step,network})=>{
+          handler: async( {files},{step,network}: Tool.Options<AgentState>)=>{
             const newFiles = await step?.run('createOrUpdateFiles', async () =>{
               try{
                 const updatedFiles =network.state.data.files || {};
@@ -124,7 +130,7 @@ export const helloWorld = inngest.createFunction(
       },
     });
 
-    const network = createNetwork({
+    const network = createNetwork<AgentState>({
       name:'coding-agent-network',
       agents:[codeAgent],
       maxIter:15,
@@ -143,14 +149,46 @@ export const helloWorld = inngest.createFunction(
     // to call your AI model.
     const result = await network.run(event.data.value);
 
-    console.log(output)
+    const isError =
+      !result.state.data.summary ||
+      Object.keys(result.state.data.files || {}).length === 0;
+
+    // console.log(output)
+
 
     const sandboxUrl = await step.run('get-sandbox-url', async () => {
 
       const sandbox =await getSandbox(sandboxId);
       const host= sandbox.getHost(3000);
       return `https://${host}`;
-    })
+    });
+
+    await step.run('save-result',async()=>{
+      if(isError){
+        return await prisma.message.create({
+          data:{
+            content:'Something went wrong.Please try again.',
+            role:'ASSISTANT',
+            type:'ERROR',
+          },
+        });
+      }
+
+      return await prisma.message.create({
+        data:{
+          content:result.state.data.summary,
+          role:'ASSISTANT',
+          type:'RESULT',
+          fragment:{
+            create:{
+              sandboxUrl:sandboxUrl,
+              title:'Fragment',
+              files:result.state.data.files,
+            }
+          }
+        },
+      })
+    });
 
   
     return {
