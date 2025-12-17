4ae258bb087d647f98d965d52057f18aa6e08447
Conquer-ai-gif <korleylahm@gmail.com>
2025-12-17 04:48:43 -0800

    11: project header

--- src/modules/projects/ui/views/project-view.tsx
+++ src/modules/projects/ui/views/project-view.tsx
@@ -2,16 +2,20 @@
 
 import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
+import {Fragment } from '@/generated/prisma/client'
 
 import {ResizableHandle,ResizablePanel,ResizablePanelGroup} from '@/components/ui/resizable'
 import { MessagesContainer } from "../components/messages-container";
 import { Suspense } from "react";
+import { ProjectHeader } from "../components/project-header";
 
 interface Props {
     projectId: string;
 }
 
  export const ProjectView=({projectId}:Props)=>{
+    
+
     const trpc = useTRPC()
     const {data:project}=useSuspenseQuery(trpc.projects.getOne.queryOptions({
         id:projectId,
@@ -24,10 +28,21 @@ interface Props {
                 <ResizablePanel
                     defaultSize ={35}
                     minSize={20}
-                    className='flex flex-colm min-h-0'
+                    className='flex flex-col min-h-0'
                 >
+                 <Suspense fallback={<p>loading project..</p>}>
+                 <ProjectHeader
+                  projectId={projectId}
+                />
+                </Suspense>
+                
                 <Suspense fallback={<p>loading messages...</p>}>
-                  <MessagesContainer projectId={projectId}/>
+                  <MessagesContainer 
+
+                  projectId={projectId}
+                  activeFragment={activeFragment}
+                  setActiveFragment={setActiveFragment}
+                  />
                 </Suspense>
                 </ResizablePanel>
                 <ResizableHandle withHandle/>
