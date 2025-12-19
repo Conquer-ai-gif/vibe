336940d82c4cff6c50a5b4a0e6cee8c98d041311
Conquer-ai-gif <korleylahm@gmail.com>
2025-12-18 19:13:05 -0800

    13: code view

--- src/modules/projects/ui/views/project-view.tsx
+++ src/modules/projects/ui/views/project-view.tsx
@@ -3,20 +3,27 @@
 import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import {Fragment } from '@/generated/prisma/client'
+import {Tabs,TabsContent,TabsList,TabsTrigger} from "@/components/ui/tabs"
 
 import {ResizableHandle,ResizablePanel,ResizablePanelGroup} from '@/components/ui/resizable'
 import { MessagesContainer } from "../components/messages-container";
-import { Suspense } from "react";
 import { ProjectHeader } from "../components/project-header";
 import { FragmentWeb } from "../components/fragment-web";
+import {FileExplorer} from ""
+
+import { Suspense, useState } from "react";
+import { EyeIcon ,CodeIcon,CrownIcon} from "lucide-react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 
 interface Props {
     projectId: string;
 }
 
  export const ProjectView=({projectId}:Props)=>{
-    
-
+    const [activeFragment,setActiveFragment] = useState<Fragment | null>(null)
+    const [tabState,setTabState] = useState<"preview" | "code">("preview")
     const trpc = useTRPC()
     const {data:project}=useSuspenseQuery(trpc.projects.getOne.queryOptions({
         id:projectId,
@@ -52,7 +59,43 @@ interface Props {
                      minSize={50}
                      className=""
                 >
-                 {!!activeFragment && <FragmentWeb data={activeFragment}/>}
+                    <Tabs
+                     className="h-full gap-y-0"
+                     defaultValue="preview"
+                     value={tabState}
+                     onValueChange={(value)=> setTabState(value as "preview" | "code")}
+                    >
+                        <div className="w-full flex items-center p-2 border-b gap-x-2">
+                            <TabsList
+                             className="h-0 p-0 rounded-md">
+                                <TabsTrigger value="preview" className="rounded-md">
+                                    <EyeIcon/> <span>Demo</span>
+                                </TabsTrigger>
+                                <TabsTrigger value="code" className="rounded-md">
+                                    <CodeIcon/> <span>Code</span>
+                                </TabsTrigger>
+                             </TabsList>
+
+                             <div className="ml-auto flex items-center gap-x-2">
+                                <Button asChild size="sm" variant={default}>
+                                    <Link href="pricing">
+                                        <CrownIcon/> Upgrade
+                                    </Link>                                
+                                </Button>
+                             </div>
+                        {/* </div> */}
+                        <TabsContent value="preview">
+                         {!!activeFragment && <FragmentWeb data={activeFragment}/>}
+
+                        </TabsContent>
+                        <TabsContent value="code" className="min-h-0">
+                               {!!activeFragment?.files && (
+                                <FileExplorer 
+                                 files={activeFragment.files as {[path:string]:string}}
+                                />
+                               )}
+                        </TabsContent>
+                    </Tabs>
                 </ResizablePanel>
             </ResizablePanelGroup>
         </div>
